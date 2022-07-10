dc5f20a964c00e8f050e09ae9d2edaba9eaa1cd8
khushi144 <khushi.19b131200@abes.ac.in>
2022-07-10 12:52:12 +0530

    Java project

---
+++ src/khushi/Song.java
@@ -0,0 +1,34 @@
+package khushi;
+
+public class Song {
+	
+		String title ;
+		double duration;
+		
+
+		public Song(String title, double duration) {
+			this.title = title;
+			this.duration = duration;
+	}
+		
+		public Song() {
+			
+		}
+		public String getTitle() {
+			return title;
+		}
+		public double getDuration() {
+			return duration;
+		}
+		
+		
+		@Override
+		public String toString() {
+		// TODO Auto-generated method stub
+		return "Song{"+
+				"title='"+ title + '\'' +
+				",duration=" + duration +
+				'}';
+		}
+
+}
